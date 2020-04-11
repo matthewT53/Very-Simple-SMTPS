5b573aa3c891c732433b491ea4c227ee82e8e01a
matthewt53 <matthewta45@gmail.com>
2020-04-11 19:33:45 +1000

    Base64 encoding now works

--- tests/base64Tests.cpp
+++ tests/base64Tests.cpp
@@ -1,6 +1,11 @@
 #include "CppUTest/TestHarness.h"
 
+#include <vector>
+#include <cstdint>
+
 #include "base64.h"
+
+using byte = uint8_t;
 
 TEST_GROUP( Base64EncodingTests )
 {
@@ -8,27 +13,39 @@ TEST_GROUP( Base64EncodingTests )
 
 TEST( Base64EncodingTests, BasicEncodingTest )
 {
-    const std::string text = "aaa";
-    const std::string encoded = SMTP::Base64::Base64_Encode(text);
-    CHECK_EQUAL("YWFh", encoded);
+    const std::vector<byte> data = { 'a', 'a', 'a' };
+    const std::string encoded = smtp::Base64::Base64_Encode( data );
+    CHECK_EQUAL( "YWFh", encoded );
 }
 
-TEST( Base64EncodingTests, PaddingTest ) 
+TEST( Base64EncodingTests, OnePaddingCharacterTest ) 
 {
-    const std::string text = "aaaa";
-    const std::string encoded = SMTP::Base64::Base64_Encode(text);
-    CHECK_EQUAL("YWFhYQo=", encoded); 
+    const std::vector<byte> data = { 'a', 'a', 'a', 'a', 'a' };
+    const std::string encoded = smtp::Base64::Base64_Encode( data );
+    CHECK_EQUAL( "YWFhYWE=", encoded );
+}
+
+TEST( Base64EncodingTests, TwoPaddingCharacterTest )
+{
+    const std::vector<byte> data = { 'a', 'a', 'a', 'a' };
+    const std::string encoded = smtp::Base64::Base64_Encode( data );
+    CHECK_EQUAL( "YWFhYQ==", encoded ); 
 }
 
 TEST( Base64EncodingTests, EmptyStringTest )
 {
-    const std::string encoded = SMTP::Base64::Base64_Encode("");
-    CHECK_EQUAL("", encoded);
+    const std::string encoded = smtp::Base64::Base64_Encode( {} );
+    CHECK_EQUAL( "", encoded );
 }
 
 TEST_GROUP( Base64UrlEncodingTests )
 {
 };
+
+TEST( Base64UrlEncodingTests, BasicEncodingTest )
+{
+    
+}
 
 TEST_GROUP( Base64DecodingTests )
 {
