3cf1aa110b7c4196ba6e3ee6b8e79216f75dba19
matthewt53 <matthewta45@gmail.com>
2020-04-12 13:33:58 +1000

    Base64Url encoding works

--- src/base64.cpp
+++ src/base64.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <cstdint>
+#include <algorithm>
 
 #include "base64.h"
 
@@ -13,7 +14,7 @@ static const std::string kTable = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrs
 static const std::string kTableUrl = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
 
 const std::string DoBase64Encode(const std::vector<byte> &data, const std::string &table);
-const std::string DoEncodeBlock(byte arr[], const std::string &table, int padding = 0);
+const std::string DoBase64EncodeBlock(byte arr[], const std::string &table, int padding = 0);
 
 const std::string smtp::Base64::Base64_Encode(const std::vector<byte> &data)
 {
@@ -29,8 +30,8 @@ const std::string smtp::Base64::Base64_Decode(const std::vector<byte> &data)
 
 const std::string smtp::Base64::Base64Url_Encode(const std::vector<byte> &data)
 {
-    //TODO
-    std::string result;
+    std::string result = DoBase64Encode(data, kTableUrl);
+    result.erase(std::remove(result.begin(), result.end(), '='), result.end());
     return result;
 }
 
@@ -55,14 +56,14 @@ const std::string DoBase64Encode(const std::vector<byte> &data, const std::strin
 
         if (i % 3 == 0 || i == n)
         {
-            result += DoEncodeBlock(arr, table, i % 3);
+            result += DoBase64EncodeBlock(arr, table, i % 3);
         }
     }
 
     return result;
 }
 
-const std::string DoEncodeBlock(byte arr[], const std::string &table, int padding)
+const std::string DoBase64EncodeBlock(byte arr[], const std::string &table, int padding)
 {
     int i1 = (arr[0] & 0xfc) >> 2;
     int i2 = ((arr[0] & 0x3) << 4) | ((arr[1] & 0xf0) >> 4);
--- tests/base64Tests.cpp
+++ tests/base64Tests.cpp
@@ -15,6 +15,7 @@ TEST( Base64EncodingTests, BasicEncodingTest )
 {
     const std::vector<byte> data = { 'a', 'a', 'a' };
     const std::string encoded = smtp::Base64::Base64_Encode( data );
+
     CHECK_EQUAL( "YWFh", encoded );
 }
 
@@ -22,6 +23,7 @@ TEST( Base64EncodingTests, OnePaddingCharacterTest )
 {
     const std::vector<byte> data = { 'a', 'a', 'a', 'a', 'a' };
     const std::string encoded = smtp::Base64::Base64_Encode( data );
+
     CHECK_EQUAL( "YWFhYWE=", encoded );
 }
 
@@ -29,7 +31,17 @@ TEST( Base64EncodingTests, TwoPaddingCharacterTest )
 {
     const std::vector<byte> data = { 'a', 'a', 'a', 'a' };
     const std::string encoded = smtp::Base64::Base64_Encode( data );
+
     CHECK_EQUAL( "YWFhYQ==", encoded ); 
+}
+
+TEST( Base64EncodingTests, TypicalExampleTest )
+{
+    const std::string text = "what does it take for a boar to soar?";
+    const std::vector<byte> data( text.begin(), text.end() );
+    const std::string encoded = smtp::Base64::Base64_Encode( data );
+
+    CHECK_EQUAL( "d2hhdCBkb2VzIGl0IHRha2UgZm9yIGEgYm9hciB0byBzb2FyPw==", encoded );
 }
 
 TEST( Base64EncodingTests, EmptyStringTest )
@@ -44,7 +56,20 @@ TEST_GROUP( Base64UrlEncodingTests )
 
 TEST( Base64UrlEncodingTests, BasicEncodingTest )
 {
-    
+    const std::string text = "hey mate how are you doing? good day today?";
+    const std::vector<byte> data( text.begin(), text.end() );
+    const std::string encoded = smtp::Base64::Base64Url_Encode( data );
+
+    CHECK_EQUAL( "aGV5IG1hdGUgaG93IGFyZSB5b3UgZG9pbmc_IGdvb2QgZGF5IHRvZGF5Pw", encoded );
+}
+
+TEST( Base64UrlEncodingTests, PaddingRemovalTest )
+{
+    const std::string text = "what is the time?";
+    const std::vector<byte> data( text.begin(), text.end() );
+    const std::string encoded = smtp::Base64::Base64Url_Encode( data );
+
+    CHECK_EQUAL( "d2hhdCBpcyB0aGUgdGltZT8", encoded );
 }
 
 TEST_GROUP( Base64DecodingTests )
