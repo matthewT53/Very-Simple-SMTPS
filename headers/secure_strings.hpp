ea248135f8eb0cb2fded92538b93889c0fdf779e
matthewt53 <matthewta45@gmail.com>
2021-04-17 18:17:00 +1000

    Updated format of secure_strings

--- headers/secure_strings.hpp
+++ headers/secure_strings.hpp
@@ -12,7 +12,8 @@ namespace smtp {
 // This allocator zeroes out the memory for the object that is allocated once
 // the object goes out of scope. This allocator is used to construct objects
 // such as vectors or strings that may contain passwords.
-template <class T> class SecureAllocator 
+template <class T> 
+class SecureAllocator 
 {
     public:
         using value_type = T;
@@ -25,24 +26,28 @@ template <class T> class SecureAllocator
         using size_type = size_t;
         using difference_type = std::ptrdiff_t;
 
-        template <class U> struct rebind { using other = SecureAllocator<U>; };
+        template <class U> 
+        struct rebind { using other = SecureAllocator<U>; };
 
         SecureAllocator() = default;
         ~SecureAllocator() = default;
 
         template <class U> SecureAllocator(const SecureAllocator<U> &other) {}
 
-        pointer allocate(size_type num_objects) {
+        pointer allocate(size_type num_objects) 
+        {
             if (num_objects > (max_size() / sizeof(T)))
-            throw std::bad_alloc();
+                throw std::bad_alloc();
             return static_cast<pointer>(new value_type[num_objects]);
         }
 
-        pointer allocate(size_type num_objects, const_void_pointer hint) {
+        pointer allocate(size_type num_objects, const_void_pointer hint) 
+        {
             return allocate(num_objects);
         }
 
-        void deallocate(pointer p, size_type num_objects) {
+        void deallocate(pointer p, size_type num_objects) 
+        {
             std::memset(p, 0x0, num_objects);
             delete[] p;
         }
