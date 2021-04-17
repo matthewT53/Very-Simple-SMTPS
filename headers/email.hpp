e1901e657d973356e8b9b655b2e2f39aa9bf0c00
matthewt53 <matthewta45@gmail.com>
2021-04-17 13:52:15 +1000

    Updated email.hpp

--- headers/email.hpp
+++ headers/email.hpp
@@ -1,6 +1,6 @@
 /*
-	Code to send an email with attachments using LIBCURL
-	Supports SMTPS only.
+    Code to send an email with attachments using LIBCURL
+    Supports SMTPS only.
 */
 
 #ifndef SMTP_EMAIL_H
@@ -18,53 +18,53 @@ namespace smtp
 
 class Email
 {
-	public:
-		// default constructor
-		Email();
+    public:
+        // default constructor
+        Email();
 
-		inline void setTo( const std::string &p_to ) { m_to = p_to; }
-		inline void setFrom( const std::string &p_from ) { m_from = p_from; }
-		inline void setCc( const std::string &p_cc ) { m_cc = p_cc; }
-		inline void setSubject( const std::string &p_subject ) { m_subject = p_subject; }
-		inline void setBody( const std::string &p_body ) { m_body = p_body; m_mime->addMessage(m_body); }
+        inline void setTo( const std::string &p_to ) { m_to = p_to; }
+        inline void setFrom( const std::string &p_from ) { m_from = p_from; }
+        inline void setCc( const std::string &p_cc ) { m_cc = p_cc; }
+        inline void setSubject( const std::string &p_subject ) { m_subject = p_subject; }
+        inline void setBody( const std::string &p_body ) { m_body = p_body; m_mime->addMessage(m_body); }
 
-		inline void setSmtpUsername( const std::string &p_user ) { m_smtp_user = p_user; }
-		inline void setSmtpPassword( const std::string &p_password ) { m_smtp_password = p_password; }
-		inline void setSmtpHost( const std::string &p_hostname ) { m_smtp_host = p_hostname; }
-		
-		inline void addAttachment( const std::string &p_attachment ) { m_mime->addAttachment(p_attachment); }
-		inline void removeAttachment( const std::string &p_attachment ) { m_mime->removeAttachment(p_attachment); }
-		inline void setMimeDocument( std::unique_ptr<smtp::IMime> &p_mime ) { m_mime = std::move(p_mime); }
+        inline void setSmtpUsername( const std::string &p_user ) { m_smtp_user = p_user; }
+        inline void setSmtpPassword( const std::string &p_password ) { m_smtp_password = p_password; }
+        inline void setSmtpHost( const std::string &p_hostname ) { m_smtp_host = p_hostname; }
+        
+        inline void addAttachment( const std::string &p_attachment ) { m_mime->addAttachment(p_attachment); }
+        inline void removeAttachment( const std::string &p_attachment ) { m_mime->removeAttachment(p_attachment); }
+        inline void setMimeDocument( std::unique_ptr<smtp::IMime> &p_mime ) { m_mime = std::move(p_mime); }
 
-		void clear();
-		void send() const;
+        void clear();
+        void send() const;
 
-	private:
-		// smtp information
-		smtp::secure_string m_smtp_user;
-		smtp::secure_string m_smtp_password;
-		smtp::secure_string m_smtp_host;
+    private:
+        // smtp information
+        smtp::secure_string m_smtp_user;
+        smtp::secure_string m_smtp_password;
+        smtp::secure_string m_smtp_host;
 
-		// email data
-		std::string m_to;
-		std::string m_from;
-		std::string m_cc;
-		std::string m_subject;
-		std::string m_body;
+        // email data
+        std::string m_to;
+        std::string m_from;
+        std::string m_cc;
+        std::string m_subject;
+        std::string m_body;
 
-		// Mime document with possible attachments
-		std::unique_ptr<smtp::IMime> m_mime;
+        // Mime document with possible attachments
+        std::unique_ptr<smtp::IMime> m_mime;
 
-		std::vector<std::string> build() const;
-		static std::size_t payloadCallback(void *ptr, size_t size, size_t nmemb, void *userp);
+        std::vector<std::string> build() const;
+        static std::size_t payloadCallback(void *ptr, size_t size, size_t nmemb, void *userp);
 
-	friend std::ostream &operator<<(std::ostream &p_out, Email &p_email)
-	{
-		const std::vector<std::string> &email_contents = p_email.build();
-		std::string result;
-		for (const auto &line : email_contents) result += line;
-		return p_out << result;
-	}
+    friend std::ostream &operator<<(std::ostream &p_out, Email &p_email)
+    {
+        const std::vector<std::string> &email_contents = p_email.build();
+        std::string result;
+        for (const auto &line : email_contents) result += line;
+        return p_out << result;
+    }
 };
 
 }
--- src/base64.cpp
+++ src/base64.cpp
@@ -13,12 +13,12 @@ static const std::string kTable = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrs
 static const std::string kTableUrl = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
 
 static const std::unordered_map<smtp::byte, smtp::byte> kDecodeTable = {
-	{'A', 0x0}, {'B', 0x1}, {'C', 0x2}, {'D', 0x3}, {'E', 0x4}, {'F', 0x5}, {'G', 0x6}, {'H', 0x7}, {'I', 0x8}, {'J', 0x9}, {'K', 0xa}, {'L', 0xb}, 
-	{'M', 0xc}, {'N', 0xd}, {'O', 0xe}, {'P', 0xf}, {'Q', 0x10}, {'R', 0x11}, {'S', 0x12}, {'T', 0x13}, {'U', 0x14}, {'V', 0x15}, {'W', 0x16}, 
-	{'X', 0x17}, {'Y', 0x18}, {'Z', 0x19}, {'a', 0x1a}, {'b', 0x1b}, {'c', 0x1c}, {'d', 0x1d}, {'e', 0x1e}, {'f', 0x1f}, {'g', 0x20}, {'h', 0x21}, 
-	{'i', 0x22}, {'j', 0x23}, {'k', 0x24}, {'l', 0x25}, {'m', 0x26}, {'n', 0x27}, {'o', 0x28}, {'p', 0x29}, {'q', 0x2a}, {'r', 0x2b}, {'s', 0x2c}, 
-	{'t', 0x2d}, {'u', 0x2e}, {'v', 0x2f}, {'w', 0x30}, {'x', 0x31}, {'y', 0x32}, {'z', 0x33}, {'0', 0x34}, {'1', 0x35}, {'2', 0x36}, {'3', 0x37}, 
-	{'4', 0x38}, {'5', 0x39}, {'6', 0x3a}, {'7', 0x3b}, {'8', 0x3c}, {'9', 0x3d}, {'-', 0x3e}, {'_', 0x3f}, {'+', 0x3e}, {'/', 0x3f}, {'=', 0x0},
+    {'A', 0x0}, {'B', 0x1}, {'C', 0x2}, {'D', 0x3}, {'E', 0x4}, {'F', 0x5}, {'G', 0x6}, {'H', 0x7}, {'I', 0x8}, {'J', 0x9}, {'K', 0xa}, {'L', 0xb}, 
+    {'M', 0xc}, {'N', 0xd}, {'O', 0xe}, {'P', 0xf}, {'Q', 0x10}, {'R', 0x11}, {'S', 0x12}, {'T', 0x13}, {'U', 0x14}, {'V', 0x15}, {'W', 0x16}, 
+    {'X', 0x17}, {'Y', 0x18}, {'Z', 0x19}, {'a', 0x1a}, {'b', 0x1b}, {'c', 0x1c}, {'d', 0x1d}, {'e', 0x1e}, {'f', 0x1f}, {'g', 0x20}, {'h', 0x21}, 
+    {'i', 0x22}, {'j', 0x23}, {'k', 0x24}, {'l', 0x25}, {'m', 0x26}, {'n', 0x27}, {'o', 0x28}, {'p', 0x29}, {'q', 0x2a}, {'r', 0x2b}, {'s', 0x2c}, 
+    {'t', 0x2d}, {'u', 0x2e}, {'v', 0x2f}, {'w', 0x30}, {'x', 0x31}, {'y', 0x32}, {'z', 0x33}, {'0', 0x34}, {'1', 0x35}, {'2', 0x36}, {'3', 0x37}, 
+    {'4', 0x38}, {'5', 0x39}, {'6', 0x3a}, {'7', 0x3b}, {'8', 0x3c}, {'9', 0x3d}, {'-', 0x3e}, {'_', 0x3f}, {'+', 0x3e}, {'/', 0x3f}, {'=', 0x0},
 };
 
 const std::string DoBase64Encode( const std::vector<smtp::byte> &data, const std::string &table );
@@ -29,117 +29,117 @@ const std::vector<smtp::byte> DoBase64DecodeBlock( const std::string &data_block
 
 const std::string smtp::Base64::Base64Encode( const std::vector<smtp::byte> &data )
 {
-	return DoBase64Encode( data, kTable );
+    return DoBase64Encode( data, kTable );
 }
 
 const std::vector<smtp::byte> Base64::Base64Decode(const std::string &data)
 {
-	return DoBase64Decode( data );
+    return DoBase64Decode( data );
 }
 
 const std::string Base64::Base64UrlEncode(const std::vector<smtp::byte> &data, bool keep_padding)
 {
-	std::string result = DoBase64Encode( data, kTableUrl );
-	if ( !keep_padding )
-	{
-		result.erase(std::remove( result.begin(), result.end(), '='), result.end() );
-	}
-	return result;
+    std::string result = DoBase64Encode( data, kTableUrl );
+    if ( !keep_padding )
+    {
+        result.erase(std::remove( result.begin(), result.end(), '='), result.end() );
+    }
+    return result;
 }
 
 const std::vector<smtp::byte> Base64::Base64UrlDecode(const std::string &data)
 {
-	return DoBase64Decode( data );
+    return DoBase64Decode( data );
 }
 
 const std::string DoBase64Encode(const std::vector<smtp::byte> &data, const std::string &table)
 {
-	std::string result;
+    std::string result;
 
-	int i = 0;
-	int n = static_cast<int>(data.size());
-	
-	std::vector<smtp::byte> arr(3, 0x0);
+    int i = 0;
+    int n = static_cast<int>(data.size());
+    
+    std::vector<smtp::byte> arr(3, 0x0);
 
-	for (smtp::byte b : data)
-	{
-		arr[i++ % 3] = b;
+    for (smtp::byte b : data)
+    {
+        arr[i++ % 3] = b;
 
-		if (i % 3 == 0 || i == n)
-		{
-			result += DoBase64EncodeBlock(arr, table, i % 3);
-			std::fill(arr.begin(), arr.end(), 0x0);
-		}
-	}
+        if (i % 3 == 0 || i == n)
+        {
+            result += DoBase64EncodeBlock(arr, table, i % 3);
+            std::fill(arr.begin(), arr.end(), 0x0);
+        }
+    }
 
-	return result;
+    return result;
 }
 
 // Base64 encodes a block of plaintext. 
 // It is assumed that data_block contains a maximum of 3 smtp::bytes in size. 
 const std::string DoBase64EncodeBlock(const std::vector<smtp::byte> &data_block, const std::string &table, int padding)
 {
-	int i1 = (data_block[0] & 0xfc) >> 2;
-	int i2 = ((data_block[0] & 0x3) << 4) | ((data_block[1] & 0xf0) >> 4);
-	int i3 = ((data_block[1] & 0xf) << 2) | ((data_block[2] & 0xc0) >> 6);
-	int i4 = (data_block[2] & 0x3f);
+    int i1 = (data_block[0] & 0xfc) >> 2;
+    int i2 = ((data_block[0] & 0x3) << 4) | ((data_block[1] & 0xf0) >> 4);
+    int i3 = ((data_block[1] & 0xf) << 2) | ((data_block[2] & 0xc0) >> 6);
+    int i4 = (data_block[2] & 0x3f);
 
-	std::string s(4, 0x00);
-	s[0] = table[i1]; 
-	s[1] = table[i2];
-	s[2] = (padding == 1) ? '=' : table[i3];
-	s[3] = (padding >= 1) ? '=' : table[i4];
-	return s;
+    std::string s(4, 0x00);
+    s[0] = table[i1]; 
+    s[1] = table[i2];
+    s[2] = (padding == 1) ? '=' : table[i3];
+    s[3] = (padding >= 1) ? '=' : table[i4];
+    return s;
 }
 
 const std::vector<smtp::byte> DoBase64Decode(const std::string &data)
 {
-	std::vector<smtp::byte> result;
+    std::vector<smtp::byte> result;
 
-	int i = 0;
-	int n = static_cast<int>( data.length() );
-	std::string arr(4, 0x0);
+    int i = 0;
+    int n = static_cast<int>( data.length() );
+    std::string arr(4, 0x0);
 
-	for (smtp::byte b : data)
-	{
-		arr[i++ % 4] = b;
+    for (smtp::byte b : data)
+    {
+        arr[i++ % 4] = b;
 
-		if (i % 4 == 0 || i == n)
-		{
-			const std::vector<smtp::byte> decoded = DoBase64DecodeBlock(arr, i % 4);
-			result.insert(result.end(), decoded.begin(), decoded.end());
-		}
-	}
+        if (i % 4 == 0 || i == n)
+        {
+            const std::vector<smtp::byte> decoded = DoBase64DecodeBlock(arr, i % 4);
+            result.insert(result.end(), decoded.begin(), decoded.end());
+        }
+    }
 
-	return result;
+    return result;
 }
 
 // This function base64 decodes a block of 4 base64 encoded characters.
 // It is assumed that data_block is 4 smtp::bytes in size.
 const std::vector<smtp::byte> DoBase64DecodeBlock(const std::string &data_block, int rem_chars)
 {
-	std::vector<smtp::byte> result(3);
+    std::vector<smtp::byte> result(3);
 
-	smtp::byte i1 = kDecodeTable.find(data_block[0])->second;
-	smtp::byte i2 = kDecodeTable.find(data_block[1])->second;
-	smtp::byte i3 = kDecodeTable.find(data_block[2])->second;
-	smtp::byte i4 = kDecodeTable.find(data_block[3])->second;
+    smtp::byte i1 = kDecodeTable.find(data_block[0])->second;
+    smtp::byte i2 = kDecodeTable.find(data_block[1])->second;
+    smtp::byte i3 = kDecodeTable.find(data_block[2])->second;
+    smtp::byte i4 = kDecodeTable.find(data_block[3])->second;
 
-	result[0] = ((i1 & 0x3f) << 2) | ((i2 & 0x30) >> 4);
-	result[1] = ((i2 & 0xf) << 4) | ((i3 & 0x3c) >> 2);
-	result[2] = ((i3 & 0x3) << 6) | i4;
+    result[0] = ((i1 & 0x3f) << 2) | ((i2 & 0x30) >> 4);
+    result[1] = ((i2 & 0xf) << 4) | ((i3 & 0x3c) >> 2);
+    result[2] = ((i3 & 0x3) << 6) | i4;
 
-	size_t padding_pos = data_block.find_first_of("=");
-	int n_chars = (padding_pos == std::string::npos) ? rem_chars : static_cast<int>( padding_pos );
+    size_t padding_pos = data_block.find_first_of("=");
+    int n_chars = (padding_pos == std::string::npos) ? rem_chars : static_cast<int>( padding_pos );
 
-	// Padding will either be 2 or 3, since it is not possible to have 1 smtp::byte
-	if (n_chars > 0)
-	{
-		int num_remove = 4 - n_chars;
-		result.resize(3 - num_remove);
-	}
+    // Padding will either be 2 or 3, since it is not possible to have 1 smtp::byte
+    if (n_chars > 0)
+    {
+        int num_remove = 4 - n_chars;
+        result.resize(3 - num_remove);
+    }
 
-	return result;
+    return result;
 }
 
 }
