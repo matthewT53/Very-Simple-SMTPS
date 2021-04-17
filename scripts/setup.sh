924943e6db0573b4bbaac0ed69799b9cf500f32d
matthewt53 <matthewta45@gmail.com>
2021-04-17 13:33:11 +1000

    Updated setup script to pass appveyor build

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # Note: If libcurl4-gnutls-dev cannot be found then install libcurl4-openssl-dev instead and vice versa.
-sudo apt-get --assume-yes --fix-missing install python3 python3-pip python3-setuptools python3-wheel ninja-build cpputest libcurl4 libcurl4-gnutls-dev
+sudo apt-get --assume-yes --fix-missing install python3 python3-pip python3-setuptools python3-wheel ninja-build cpputest libcurl4 libcurl4-openssl-dev
 sudo pip3 install meson
