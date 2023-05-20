391a282b72c4b80c09ea68278648c11e0b010089
matthewt53 <matthewta45@gmail.com>
2023-05-20 13:50:37 +1000

    updated build script to use conan

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# conan install -of .conan . --build missing
-# conan build -of .conan .
-
-meson build && cd build
-ninja
+conan install -of .conan . --build missing
+conan build -of .conan .
