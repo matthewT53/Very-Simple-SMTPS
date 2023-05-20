b91232185d832ca2ed2fca961ec1baa243f0ba91
matthewt53 <matthewta45@gmail.com>
2023-05-20 21:32:08 +1000

    migrated base64 and secure strings

--- tests/utils/secure_strings_tests.cpp
+++ tests/utils/secure_strings_tests.cpp
@@ -1,41 +1,41 @@
 #include <cstring>
 #include <iostream>
 
-#include "CppUTest/TestHarness.h"
+#include "doctest/doctest.h"
 
-#include "secure_strings.hpp"
+#include "utils/secure_strings.hpp"
 
-TEST_GROUP(SecureStringsTests){};
+TEST_SUITE("Secure strings") {
+  TEST_CASE("Clears sensitive string buffer on destruction") {
+    const std::string &expected = "this is a password";
+    smtp::secure_string *password = new smtp::secure_string(expected);
 
-TEST(SecureStringsTests, StringTest) {
-  const std::string &expected = "this is a password";
-  smtp::secure_string *password = new smtp::secure_string(expected);
+    void *ptr = password->data();
+    CHECK_TRUE(std::memcmp(password->data(), expected.data(), expected.size()) == 0);
 
-  void *ptr = password->data();
-  CHECK_TRUE(std::memcmp(password->data(), expected.data(), expected.size()) == 0);
+    int len = static_cast<int>(expected.size());
+    char arr[len] = {0x0};
+    std::memset(arr, 0x0, len);
 
-  int len = static_cast<int>(expected.size());
-  char arr[len] = {0x0};
-  std::memset(arr, 0x0, len);
+    delete password;
 
-  delete password;
+    // Ensure that each byte is different at this memory location and that the
+    // data here does not form the original string. This is the best we can do
+    // because once the memory is deallocated, the memory address may have been
+    // allocated to something else.
+    auto memory_check = [&]() -> bool {
+      bool result = true;
+      char *mem = static_cast<char *>(ptr);
 
-  // Ensure that each byte is different at this memory location and that the
-  // data here does not form the original string. This is the best we can do
-  // because once the memory is deallocated, the memory address may have been
-  // allocated to something else.
-  auto memory_check = [&]() -> bool {
-    bool result = true;
-    char *mem = static_cast<char *>(ptr);
-
-    for (std::size_t i = 0; i < expected.length(); i++) {
-      if (expected[i] == mem[i]) {
-        result = false;
+      for (std::size_t i = 0; i < expected.length(); i++) {
+        if (expected[i] == mem[i]) {
+          result = false;
+        }
       }
-    }
 
-    return result;
-  };
+      return result;
+    };
 
-  CHECK_TRUE(memory_check());
+    REQUIRE(memory_check() == true);
+  }
 }
