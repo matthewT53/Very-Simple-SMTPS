4013c75ded0e71f88a31cd0fe97448f78252d596
matthewt53 <matthewta45@gmail.com>
2023-05-29 01:17:54 +1000

    refactor base64 code

--- src/utils/base64/base64.hpp
+++ src/utils/base64/base64.hpp
@@ -10,12 +10,11 @@ using byte = uint8_t;
 
 class Base64 {
 public:
-  static const std::string Base64Encode(const std::vector<uint8_t> &data);
-  static const std::vector<uint8_t> Base64Decode(const std::string &data);
+  static std::string Base64Encode(const std::vector<uint8_t> &data);
+  static std::vector<uint8_t> Base64Decode(const std::string &data);
 
-  static const std::string Base64UrlEncode(const std::vector<uint8_t> &data,
-                                           bool keep_padding = false);
-  static const std::vector<uint8_t> Base64UrlDecode(const std::string &data);
+  static std::string Base64UrlEncode(const std::vector<uint8_t> &data, bool keep_padding = false);
+  static std::vector<uint8_t> Base64UrlDecode(const std::string &data);
 };
 
 } // namespace smtp
