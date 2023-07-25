81004aa36e03e422e4adfc15ac21bfb4a7a4672d
matthewt53 <matthewta45@gmail.com>
2023-07-25 20:43:36 +1000

    fix email tests

--- tests/email/email_tests.cpp
+++ tests/email/email_tests.cpp
@@ -18,160 +18,197 @@ TEST_SUITE("Email tests") {
     email.setFrom("tully@gmail.com");
     email.setSubject("PWC pay rise");
     email.setCc("All the bosses at PWC");
+    email.setDate("25/07/2023 07:21:05 +1100");
     email.setBody("Hey mate, I have been working here for 5 years now, I think "
                   "its time for a pay rise.");
 
+    const std::string &expected =
+        "To: bigboss@gmail.com\r\n"
+        "From: tully@gmail.com\r\n"
+        "Cc: All the bosses at PWC\r\n"
+        "Subject: PWC pay rise\r\n"
+        "25/07/2023 07:21:05 +1100\r\n"
+        "User-Agent: Very-Simple-SMTPS\r\n"
+        "MIME-Version: 1.0\r\n"
+        "Content-Type: multipart/mixed;\r\n"
+        " boundary=\"----------030203080101020302070708\"\r\n"
+        "\r\n"
+        "This is a multi-part message in MIME format.\r\n"
+        "------------030203080101020302070708\r\n"
+        "Content-Type: text/plain; charset=utf-8; format=flowed\r\n"
+        "Content-Transfer-Encoding: 7bit\r\n"
+        "\r\n"
+        "Hey mate, I have been working here for 5 years now, I think its time for a pay rise.\r\n"
+        "------------030203080101020302070708\r\n"
+        "------------030203080101020302070708--\r\n"
+        ".\r\n";
+
     std::stringstream ss;
     ss << email;
-
-    const std::string &expected_header = "To: bigboss@gmail.com\r\n"
-                                         "From: tully@gmail.com\r\n"
-                                         "Cc: All the bosses at PWC\r\n"
-                                         "Subject: PWC pay rise\r\n";
-
-    const std::string &expected_body =
-        "\r\n"
-        "Hey mate, I have been working here for 5 years now, I think its time "
-        "for a pay rise."
-        "\r\n"
-        // This is where the attachments will go if there are any.
-        "\r\n.\r\n";
-
-    const std::string &email_contents = ss.str();
-
-    std::cout << email_contents << std::endl;
-    REQUIRE(expected_header == email_contents.substr(0, expected_header.length()));
-    REQUIRE(expected_body == email_contents.substr(
-                                 email_contents.find("\r\nHey mate, I have been working here for 5 "
-                                                     "years now, I think its time for a pay rise."),
-                                 expected_body.length()));
+    const std::string &actual = ss.str();
+    REQUIRE(expected == actual);
   }
 
-  // TEST_CASE("Add attachment test") {
-  //   smtp::EmailParams params{"user", "password", "hostname"};
-  //   smtp::Email email(params);
+  TEST_CASE("Add attachment test") {
+    smtp::EmailParams params{"user", "password", "hostname"};
+    smtp::Email email(params);
 
-  //   email.setTo("bigboss@gmail.com");
-  //   email.setFrom("tully@gmail.com");
-  //   email.setSubject("PWC pay rise");
-  //   email.setCc("All the bosses at PWC");
-  //   email.setBody("Hey mate, I have been working here for 5 years now, I think "
-  //                 "its time for a pay rise.");
+    email.setTo("bigboss@gmail.com");
+    email.setFrom("tully@gmail.com");
+    email.setSubject("PWC pay rise");
+    email.setDate("25/07/2023 07:21:05 +1100");
+    email.setCc("All the bosses at PWC");
+    email.setBody("Hey mate, I have been working here for 5 years now, I think "
+                  "its time for a pay rise.");
 
-  //   smtp::Attachment attachment;
-  //   const std::string &contents = "MimeMockAttachment";
-  //   attachment.setContents(std::vector<uint8_t>(contents.begin(), contents.end()));
-  //   email.addAttachment(attachment);
+    smtp::Attachment attachment;
+    const std::string &contents = "MimeMockAttachment";
+    attachment.setContents(std::vector<uint8_t>(contents.begin(), contents.end()));
+    attachment.setFilePath("/path/test.txt");
+    email.addAttachment(attachment);
 
-  //   std::stringstream ss;
-  //   ss << email;
+    const std::string &expected =
+        "To: bigboss@gmail.com\r\n"
+        "From: tully@gmail.com\r\n"
+        "Cc: All the bosses at PWC\r\n"
+        "Subject: PWC pay rise\r\n"
+        "25/07/2023 07:21:05 +1100\r\n"
+        "User-Agent: Very-Simple-SMTPS\r\n"
+        "MIME-Version: 1.0\r\n"
+        "Content-Type: multipart/mixed;\r\n"
+        " boundary=\"----------030203080101020302070708\"\r\n"
+        "\r\n"
+        "This is a multi-part message in MIME format.\r\n"
+        "------------030203080101020302070708\r\n"
+        "Content-Type: text/plain; charset=utf-8; format=flowed\r\n"
+        "Content-Transfer-Encoding: 7bit\r\n"
+        "\r\n"
+        "Hey mate, I have been working here for 5 years now, I think its time for a pay rise.\r\n"
+        "------------030203080101020302070708\r\n"
+        "Content-Type: application/octet-stream\r\n"
+        "Content-Transfer-Encoding: base64\r\n"
+        "Content-Disposition: attachment;\r\n"
+        " filename=test.txt\r\n"
+        "\r\n"
+        "TWltZU1vY2tBdHRhY2htZW50\r\n"
+        "\r\n"
+        "------------030203080101020302070708\r\n"
+        "------------030203080101020302070708--\r\n"
+        ".\r\n";
 
-  //   const std::string &expected_header = "To: bigboss@gmail.com\r\n"
-  //                                        "From: tully@gmail.com\r\n"
-  //                                        "Cc: All the bosses at PWC\r\n"
-  //                                        "Subject: PWC pay rise\r\n";
+    std::stringstream ss;
+    ss << email;
+    const std::string &actual = ss.str();
 
-  //   const std::string &expected_body =
-  //       "\r\n"
-  //       "Hey mate, I have been working here for 5 years now, I think its time "
-  //       "for a pay rise."
-  //       "\r\n"
-  //       "\r\n"
-  //       "MimeMockAttachment"
-  //       "\r\n"
-  //       "\r\n.\r\n";
+    REQUIRE(expected == actual);
+  }
 
-  //   const std::string &email_contents = ss.str();
+  TEST_CASE("Remove attachment test") {
+    smtp::EmailParams params{"user", "password", "hostname"};
+    smtp::Email email(params);
 
-  //   REQUIRE(expected_header == email_contents.substr(0, expected_header.length()));
-  //   REQUIRE(expected_body == email_contents.substr(
-  //                                email_contents.find("\r\nHey mate, I have been working here for
-  //                                5 "
-  //                                                    "years now, I think its time for a pay
-  //                                                    rise."),
-  //                                expected_body.length()));
-  // }
+    email.setTo("bigboss@gmail.com");
+    email.setFrom("tully@gmail.com");
+    email.setSubject("PWC pay rise");
+    email.setDate("25/07/2023 07:21:05 +1100");
+    email.setCc("All the bosses at PWC");
+    email.setBody("Hey mate, I have been working here for 5 years now, I think "
+                  "its time for a pay rise.");
 
-  // TEST_CASE("Remove attachment test") {
-  //   smtp::EmailParams params{"user", "password", "hostname"};
-  //   smtp::Email email(params);
+    smtp::Attachment attachment_one;
+    const std::string &contents_one = "MimeMockAttachment";
+    attachment_one.setContents(std::vector<uint8_t>(contents_one.begin(), contents_one.end()));
+    attachment_one.setFilePath("/path/test1.txt");
+    email.addAttachment(attachment_one);
 
-  //   email.setTo("bigboss@gmail.com");
-  //   email.setFrom("tully@gmail.com");
-  //   email.setSubject("PWC pay rise");
-  //   email.setCc("All the bosses at PWC");
-  //   email.setBody("Hey mate, I have been working here for 5 years now, I think "
-  //                 "its time for a pay rise.");
+    smtp::Attachment attachment_two;
+    const std::string &contents_two = "MimeMockAttachment2";
+    attachment_two.setContents(std::vector<uint8_t>(contents_two.begin(), contents_two.end()));
+    attachment_two.setFilePath("/path/test2.txt");
+    email.addAttachment(attachment_two);
 
-  //   smtp::Attachment attachment_one;
-  //   const std::string &contents_one = "MimeMockAttachment";
-  //   attachment_one.setContents(std::vector<uint8_t>(contents_one.begin(), contents_one.end()));
-  //   email.addAttachment(attachment_one);
+    email.removeAttachment("/path/test1.txt");
 
-  //   smtp::Attachment attachment_two;
-  //   const std::string &contents_two = "MimeMockAttachment2";
-  //   attachment_two.setContents(std::vector<uint8_t>(contents_two.begin(), contents_two.end()));
-  //   email.addAttachment(attachment_two);
+    const std::string &expected =
+        "To: bigboss@gmail.com\r\n"
+        "From: tully@gmail.com\r\n"
+        "Cc: All the bosses at PWC\r\n"
+        "Subject: PWC pay rise\r\n"
+        "25/07/2023 07:21:05 +1100\r\n"
+        "User-Agent: Very-Simple-SMTPS\r\n"
+        "MIME-Version: 1.0\r\n"
+        "Content-Type: multipart/mixed;\r\n"
+        " boundary=\"----------030203080101020302070708\"\r\n"
+        "\r\n"
+        "This is a multi-part message in MIME format.\r\n"
+        "------------030203080101020302070708\r\n"
+        "Content-Type: text/plain; charset=utf-8; format=flowed\r\n"
+        "Content-Transfer-Encoding: 7bit\r\n"
+        "\r\n"
+        "Hey mate, I have been working here for 5 years now, I think its time for a pay rise.\r\n"
+        "------------030203080101020302070708\r\n"
+        "Content-Type: application/octet-stream\r\n"
+        "Content-Transfer-Encoding: base64\r\n"
+        "Content-Disposition: attachment;\r\n"
+        " filename=test2.txt\r\n"
+        "\r\n"
+        "TWltZU1vY2tBdHRhY2htZW50Mg==\r\n"
+        "\r\n"
+        "------------030203080101020302070708\r\n"
+        "------------030203080101020302070708--\r\n"
+        ".\r\n";
 
-  //   email.removeAttachment(attachment_one);
+    std::stringstream ss;
+    ss << email;
+    const std::string &actual = ss.str();
 
-  //   std::stringstream ss;
-  //   ss << email;
+    REQUIRE(expected == actual);
+  }
 
-  //   const std::string &expected_header = "To: bigboss@gmail.com\r\n"
-  //                                        "From: tully@gmail.com\r\n"
-  //                                        "Cc: All the bosses at PWC\r\n"
-  //                                        "Subject: PWC pay rise\r\n";
+  TEST_CASE("Clear test") {
+    smtp::EmailParams params{"user", "password", "hostname"};
+    smtp::Email email(params);
 
-  //   const std::string &expected_body =
-  //       "\r\n"
-  //       "Hey mate, I have been working here for 5 years now, I think its time "
-  //       "for a pay rise."
-  //       "\r\n"
-  //       "\r\n"
-  //       "MimeMockAttachment2"
-  //       "\r\n"
-  //       "\r\n.\r\n";
+    email.setTo("bigboss@gmail.com");
+    email.setFrom("tully@gmail.com");
+    email.setSubject("PWC pay rise");
+    email.setCc("All the bosses at PWC");
+    email.setDate("25/07/2023 07:21:05 +1100");
+    email.setBody("Hey mate, I have been working here for 5 years now, I think "
+                  "its time for a pay rise.");
 
-  //   const std::string &email_contents = ss.str();
+    smtp::Attachment attachment_one;
+    const std::string &contents_one = "MimeMockAttachment";
+    attachment_one.setContents(std::vector<uint8_t>(contents_one.begin(), contents_one.end()));
+    email.addAttachment(attachment_one);
 
-  //   REQUIRE(expected_header == email_contents.substr(0, expected_header.length()));
-  //   REQUIRE(expected_body == email_contents.substr(
-  //                                email_contents.find("\r\nHey mate, I have been working here for
-  //                                5 "
-  //                                                    "years now, I think its time for a pay
-  //                                                    rise."),
-  //                                expected_body.length()));
-  // }
+    email.clear();
+    email.setDate("25/07/2023 07:21:05 +1100");
 
-  // TEST_CASE("Clear test") {
-  //   smtp::EmailParams params{"user", "password", "hostname"};
-  //   smtp::Email email(params);
+    const std::string &expected = "To: \r\n"
+                                  "From: \r\n"
+                                  "Cc: \r\n"
+                                  "Subject: \r\n"
+                                  "25/07/2023 07:21:05 +1100\r\n"
+                                  "User-Agent: Very-Simple-SMTPS\r\n"
+                                  "MIME-Version: 1.0\r\n"
+                                  "Content-Type: multipart/mixed;\r\n"
+                                  " boundary=\"----------030203080101020302070708\"\r\n"
+                                  "\r\n"
+                                  "This is a multi-part message in MIME format.\r\n"
+                                  "------------030203080101020302070708\r\n"
+                                  "Content-Type: text/plain; charset=utf-8; format=flowed\r\n"
+                                  "Content-Transfer-Encoding: 7bit\r\n"
+                                  "\r\n"
+                                  "\r\n"
+                                  "------------030203080101020302070708\r\n"
+                                  "------------030203080101020302070708--\r\n"
+                                  ".\r\n";
 
-  //   email.setTo("bigboss@gmail.com");
-  //   email.setFrom("tully@gmail.com");
-  //   email.setSubject("PWC pay rise");
-  //   email.setCc("All the bosses at PWC");
-  //   email.setBody("Hey mate, I have been working here for 5 years now, I think "
-  //                 "its time for a pay rise.");
+    std::stringstream ss;
+    ss << email;
+    const std::string &actual = ss.str();
 
-  //   smtp::Attachment attachment_one;
-  //   const std::string &contents_one = "MimeMockAttachment";
-  //   attachment_one.setContents(std::vector<uint8_t>(contents_one.begin(), contents_one.end()));
-  //   email.addAttachment(attachment_one);
-
-  //   email.clear();
-
-  //   std::stringstream ss;
-  //   ss << email;
-
-  //   const std::string &expected_email = "To: \r\n"
-  //                                       "From: \r\n"
-  //                                       "Cc: \r\n"
-  //                                       "Subject: \r\n";
-
-  //   const std::string &email_contents = ss.str();
-
-  //   REQUIRE(expected_email == email_contents);
-  // }
+    REQUIRE(expected == actual);
+  }
 }
