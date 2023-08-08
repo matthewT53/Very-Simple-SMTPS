ab82b2717308f85e1054fefe52662b673e093048
matthewt53 <matthewta45@gmail.com>
2023-08-09 01:01:11 +1000

    add profiles for building with conan

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-conan install -of .conan . --build missing
-conan build -of .conan .
+if [[ $1 == "release" ]]; then
+  conan install -of .conan . --build missing -pr ./profiles/release.profile
+  conan build -of .conan . -pr ./profiles/release.profile
+else
+  conan install -of .conan . --build missing -pr ./profiles/debug.profile
+  conan build -of .conan . -pr ./profiles/debug.profile
+fi
