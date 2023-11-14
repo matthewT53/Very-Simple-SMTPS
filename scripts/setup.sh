f438e5be7ef18dd64ff0616445d324560e248079
matthewt53 <matthewta45@gmail.com>
2023-11-15 00:55:43 +1100

    remove useless comment

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-# Note: If libcurl4-gnutls-dev cannot be found then install libcurl4-openssl-dev instead and vice versa.
 apt-get update && apt-get upgrade -y
 apt-get -y install python3-pip ninja-build python3.11-venv cmake build-essential
